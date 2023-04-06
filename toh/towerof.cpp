899bc24185d4739deaee520e4dfb7c5e37e80328
mihirh19 <miheerhadvani990@gmail.com>
2023-04-06 15:48:22 +0530

    update

--- toh/towerof.cpp
+++ toh/towerof.cpp
@@ -7,7 +7,7 @@ void toh(int n, char a, char b, char c)
     if (n > 0)
     {
         toh(n - 1, a, c, b);
-        cout << "move disk from   " << a << "   to  " << b << endl;
+        cout << "move disk from   " << a << "   to  " << c << endl;
         toh(n - 1, b, a, c);
     }
 }
@@ -15,7 +15,7 @@ void toh(int n, char a, char b, char c)
 int main(int argc, char const *argv[])
 {
     clock_t t = clock();
-    toh(25, 'a', 'b', 'c');
+    toh(3, 'a', 'b', 'c');
     t = clock() - t;
 
     double d = double(t) / CLOCKS_PER_SEC;
